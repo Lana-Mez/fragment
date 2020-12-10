e9d21615693f3483d1df4f30f2fbf115d6814537
User@WINDOWS-6M36B8B <https://github.com/Lana-Mez/homework.git>
2020-12-10 11:53:42 +0400

    davaleba

---
+++ app/src/main/java/com/example/notfair/buble/DashboardFragment.kt
@@ -0,0 +1,7 @@
+package com.example.notfair.buble
+
+import androidx.fragment.app.Fragment
+import com.example.notfair.R
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+}
---
+++ app/src/main/java/com/example/notfair/buble/SearchFragment.kt
@@ -0,0 +1,8 @@
+package com.example.notfair.buble
+
+import androidx.fragment.app.Fragment
+import com.example.notfair.R
+
+class SearchFragment : Fragment(R.layout.fragment_search) {
+
+}
